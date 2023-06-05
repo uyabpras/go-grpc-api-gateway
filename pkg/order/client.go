48e56de941a13bbdd10c90c3c3c851dc17c9c10d
“uyabpras” <uyabprasetyo98@gmail.com>
2023-06-05 22:48:36 +0700

    fix proto product

--- pkg/order/client.go
+++ pkg/order/client.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/uyabpras/go-grpc-api-gateway/pkg/config"
 	"github.com/uyabpras/go-grpc-api-gateway/pkg/order/pb"
+
 	"google.golang.org/grpc"
 )
 
-type ServiceClient struct{
+type ServiceClient struct {
 	Client pb.OrderServiceClient
 }
 
-func InitServiceClient (c *config.Config) pb.OrderServiceClient{
+func InitServiceClient(c *config.Config) pb.OrderServiceClient {
 	cc, err := grpc.Dial(c.OrderSvcUrl, grpc.WithInsecure())
 
 	if err != nil {
